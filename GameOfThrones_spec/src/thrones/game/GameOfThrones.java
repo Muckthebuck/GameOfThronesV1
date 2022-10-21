49b68a87720d5e69228abdd71501d919730895b9
Emily <efzhang@student.unimelb.edu.au>
2022-10-21 17:34:11 +1100

    reading properties

--- GameOfThrones_spec/src/thrones/game/GameOfThrones.java
+++ GameOfThrones_spec/src/thrones/game/GameOfThrones.java
@@ -5,7 +5,13 @@ package thrones.game;
 import ch.aplu.jcardgame.CardGame;
 
 import java.awt.*;
+import java.io.InputStream;
 import java.util.Random;
+import thrones.game.PlayerFactory.PlayerType;
+
+import java.util.Properties;
+
+
 
 @SuppressWarnings("serial")
 public class GameOfThrones extends CardGame {
@@ -16,36 +22,38 @@ public class GameOfThrones extends CardGame {
     static Random random;
     private final String version = "1.0";
     private final Table table;
+    private static final String DEFAULT_PROPERTIES_PATH = "properties/got.properties";
 
-    public GameOfThrones() {
+    public GameOfThrones(Properties properties) {
         super(700, 700, 30);
         setTitle("Game of Thrones (V" + version + ") Constructed for UofM SWEN30006 with JGameGrid (www.aplu.ch)");
         setStatusText("Initializing...");
-        table = new Table(this);
+        table = new Table(this, properties);
     }
 
     public static void main(String[] args) {
-        // System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        // final Properties properties = new Properties();
-        // properties.setProperty("watchingTime", "5000");
-        /*
-        if (args == null || args.length == 0) {
-            //  properties = PropertiesLoader.loadPropertiesFile("cribbage.properties");
-        } else {
-            //  properties = PropertiesLoader.loadPropertiesFile(args[0]);
+        String propertiesPath = DEFAULT_PROPERTIES_PATH;
+        if (args.length > 0) {
+            propertiesPath = args[0];
         }
+        final Properties properties = PropertiesLoader.loadPropertiesFile(propertiesPath);
 
+        //System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        properties.setProperty("watchingTime", "5000");
         String seedProp = properties.getProperty("seed");  //Seed property
         if (seedProp != null) { // Use property seed
-			  seed = Integer.parseInt(seedProp);
+            seed = Integer.parseInt(seedProp);
         } else { // and no property
-			  seed = new Random().nextInt(); // so randomise
+            seed = new Random().nextInt(); // so randomise
         }
-        */
-        GameOfThrones.seed = 130006;
+
+        //GameOfThrones.seed = 130006;
         System.out.println("Seed = " + seed);
+
+
         GameOfThrones.random = new Random(seed);
-        new GameOfThrones();
+
+        new GameOfThrones(properties);
     }
 
 }
